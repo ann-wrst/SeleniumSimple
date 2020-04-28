e5f5c465783597ddfa693de0858831d6cdda5e33
ann-wrst <58138614+ann-wrst@users.noreply.github.com>
2020-04-28 22:31:16 +0300

    Add files via upload

---
+++ TrelloTests/src/test/java/BAsicSelenium.java
@@ -0,0 +1,14 @@
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class BAsicSelenium extends TestManager{
+    @Test
+    public void openHomePage (){
+        driver.get("https://trello.com/");
+        String title = driver.getTitle();
+        Assertions.assertEquals(title, "Trello");
+
+    }
+}
